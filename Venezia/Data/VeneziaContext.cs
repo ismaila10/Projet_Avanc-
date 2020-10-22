ae53e0414cda1c5027fa8eabe2ca36c21fa28fad
ismaila <lodia.ismaila@gmail.com>
2020-10-22 12:25:29 +0200

    configurer Identity et lier avec EF

--- Venezia/Data/VeneziaContext.cs
+++ Venezia/Data/VeneziaContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
@@ -9,7 +11,7 @@ using Venezia.Models;
 
 namespace Venezia.Data
 {
-    public class VeneziaContext : DbContext
+    public class VeneziaContext : IdentityDbContext<IdentityUser>
     {
         public static readonly ILoggerFactory SqlLogger = LoggerFactory.Create(builder => { builder.AddConsole(); });
 
